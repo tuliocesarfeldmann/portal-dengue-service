ab4659ad696e33f2ffe1b3c12219bd8d306c1c7d
Michel Felipe Krohn Forsch <michel.forsch@acad.ufsm.br>
2024-03-26 20:38:05 -0300

    Início do projeto

---
+++ src/main/java/com/ufsm/portaldengue/PortaldengueApplication.java
@@ -0,0 +1,13 @@
+package com.ufsm.portaldengue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortaldengueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortaldengueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ufsm/portaldengue/PortaldengueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufsm.portaldengue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortaldengueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
