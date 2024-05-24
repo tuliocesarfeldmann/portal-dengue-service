7bc3a0177309ccccc8effcaf3ea401ecd273ccf2
Túlio César Feldmann <90226273+tuliocesarfeldmann@users.noreply.github.com>
2024-05-23 22:12:49 -0300

    Adicionando titulo pro informativo

--- src/main/java/com/ufsm/portaldengue/model/entity/Informative.java
+++ src/main/java/com/ufsm/portaldengue/model/entity/Informative.java
@@ -14,6 +14,9 @@ public class Informative {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "titulo")
+    private String title;
+
     @Column(name = "descricao")
     private String description;
 
